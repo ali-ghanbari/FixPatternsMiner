703554ce7b8f04b2a39d10c80328d157434ff954
ali-ghanbari <ali.ghanbari@utdallas.edu>
2018-11-24 20:42:47 -0600

    bug fix

--- src/main/java/edu/utdallas/fpm/pattern/rules/prapr_specializations/InlineConstantMutatorRule.java
+++ src/main/java/edu/utdallas/fpm/pattern/rules/prapr_specializations/InlineConstantMutatorRule.java
@@ -2,11 +2,18 @@ package edu.utdallas.fpm.pattern.rules.prapr_specializations;
 
 import edu.utdallas.fpm.pattern.rules.ConstantReplacementRule;
 import edu.utdallas.fpm.pattern.rules.Rule;
+import edu.utdallas.fpm.pattern.rules.util.SerializableLiteral;
 
 public class InlineConstantMutatorRule implements Rule {
     public static InlineConstantMutatorRule build(final ConstantReplacementRule crr) {
         final Object src = crr.getSourceLiteral().getValue();
-        final Object dst = crr.getDestinationLiteral().getValue();
+        final SerializableLiteral serializableLiteral = crr.getDestinationLiteral();
+        final Object dst;
+        if (serializableLiteral != null) {
+            dst = serializableLiteral.getValue();
+        } else {
+            dst = null;
+        }
         if (src != null && dst != null) {
             if (src instanceof Double && dst instanceof Double) {
                 final double sd = ((Double) src).doubleValue();
